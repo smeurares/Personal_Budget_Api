79b3755714166d83b0fc9daff3620b3a7aafebcc
smeurares <smeurares@gmail.com>
2021-07-12 14:20:38 +0300

    get envelopes by id

--- routes/envelopes.js
+++ routes/envelopes.js
@@ -13,3 +13,12 @@ envelopesRouter.post('/', (req, res, next) => {
     console.log(newBudget)
     res.status(201).send(newBudget)
 })
+
+envelopesRouter.get('/:envelopeId', async (req, res, next) => {
+  const envelopeId = req.params.envelopeId;
+  if(envelopeId > 0 && envelopeId < envelopes.length) {
+    res.send(envelopes[envelopeId - 1])
+  } else{
+    res.status(400).send({message: 'ID not found'});
+  }
+})

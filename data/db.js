955c80b11ee7c6232c46b098e8541be0e888b5fc
smeurares <smeurares@gmail.com>
2021-07-14 13:00:53 +0300

    transfer complete

--- data/db.js
+++ data/db.js
@@ -2,86 +2,88 @@ const faker = require("faker");
 const casual = require("casual");
 
 let envelopes = [
-    {
-        id: 1,
-        title: 'groceries',
-        budget: 200
-    },
-    {
-        id: 2,
-        title: 'health',
-        budget: 400
-    },
-    {
-        id: 3,
-        title: 'household',
-        budget: 300
-    },
-    {
-        id: 4,
-        title: 'vices',
-        budget: 50
-    },
-    {
-        id: 5,
-        title: 'rent',
-        budget: 500
-    },
-    {
-        id: 6,
-        title: 'business',
-        budget: 2000
-    },
-    {
-        id: 7,
-        title: 'exentricity',
-        budget: 5000
-    },
-    {
-        id: 8,
-        title: 'legal',
-        budget: 7000
-    },
-]
+  {
+    id: 1,
+    title: "groceries",
+    budget: 200,
+  },
+  {
+    id: 2,
+    title: "health",
+    budget: 400,
+  },
+  {
+    id: 3,
+    title: "household",
+    budget: 300,
+  },
+  {
+    id: 4,
+    title: "vices",
+    budget: 50,
+  },
+  {
+    id: 5,
+    title: "rent",
+    budget: 500,
+  },
+  {
+    id: 6,
+    title: "business",
+    budget: 2000,
+  },
+  {
+    id: 7,
+    title: "exentricity",
+    budget: 5000,
+  },
+  {
+    id: 8,
+    title: "legal",
+    budget: 7000,
+  },
+];
 
 const getNewId = (array) => {
   if (array.length > 0) {
-      return array[array.length - 1].id + 1
+    return array[array.length - 1].id + 1;
   } else {
-      return 1
+    return 1;
   }
-}
+};
 
+const findById = (id) => {
+  const object = envelopes.find((envelope) => envelope.id === parseInt(id));
+
+  return object;
+};
 
 const addEnvelope = (budget, category) => {
   const newEnvelope = {
     id: getNewId(envelopes),
     title: category,
-    budget: budget
-  }
-  if(newEnvelope) {
-    envelopes.push(newEnvelope)
+    budget: budget,
+  };
+  if (newEnvelope) {
+    envelopes.push(newEnvelope);
     return envelopes;
   }
 };
 
 const updateBudget = (budget, id) => {
   envelopes[id].budget = budget;
-  return envelopes[id]
-}
-
-
+  return envelopes[id];
+};
 
 const createBudget = (budget) => {
   const envelopesBudget = allEnvelopes.map((category) => {
-
     const budgetForEnvelope = budget / categories.length;
     amount.money = budgetForEnvelope;
     createEnvelope(amount.money, amount.category);
   });
 
-   const definedBudget = budget
-   console.log(envelopesBudget)
+  const definedBudget = budget;
+  console.log(envelopesBudget);
   return {
     id: `${budgetId++}`,
     budget: definedBudget,
@@ -91,8 +93,14 @@ const createBudget = (budget) => {
 
 const deleteEnvelope = (id) => {
   const envelopeId = Number(id);
-  envelopes = envelopes.filter(envelope => envelope.id !== envelopeId)
-  return envelopes
-}
+  envelopes = envelopes.filter((envelope) => envelope.id !== envelopeId);
+  return envelopes;
+};
 
-module.exports = { envelopes, addEnvelope, updateBudget,deleteEnvelope };
+module.exports = {
+  envelopes,
+  addEnvelope,
+  updateBudget,
+  deleteEnvelope,
+  findById,
+};
